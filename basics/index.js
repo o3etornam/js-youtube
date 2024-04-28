dbfc4a151dd43360a9279132e63fea7e2a1e18b2
o3etornam <03etornam@gmail.com>
2024-04-28 08:51:08 +0000

    js basics

---
+++ basics/index.js
@@ -0,0 +1,15 @@
+// console.log('hello world');
+
+// document.getElementById('myH1').textContent = 'Hello';
+// document.getElementById('myP').textContent = 'i like pizza'
+
+// let age = 25;
+// console.log(age)
+
+// let username;
+// document.getElementById('mySubmit').onclick = () => {
+//     username = document.getElementById('myText').value;
+//     document.getElementById('myH1').textContent = `Welcome, ${username}`
+// } 
+
+const PI = Math.PI  
